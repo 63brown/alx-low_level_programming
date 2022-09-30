aea560aae5bf9b63bc6da473b69200c31d859566
Mumbi1 <mutheemumbi01@gmail.com>
2022-09-30 11:33:41 -0700

    Task 0

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * _strcat - appends src string to the dest
  *
@@ -11,12 +10,5 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *ptr = dest + strlen(dest);
-
-	while (*src != '\0')
-	{
-		*ptr++ = *src++;
-	}
-	*ptr = '\0';
-	return (dest);
+	
 }
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -9,12 +9,5 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
 
-	while (str[i] != '\0')
-	{
-		putchar(str[i]);
-		i++;
-	}
-	putchar('\n');
 }
