f3fa4c550f930c0840b70ddedd0a9c83c5732b33
63brown <mutheemumbi01@gmail.com>
2023-04-13 19:38:16 +0300

    searching algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,16 +12,15 @@
 int linear_search(int *array, size_t size, int value)
 {
 	unsigned int i;
-	
-	if (array == NULL);
+
+	if (array == NULL)
 	{
 		return (-1);
 	}
-	
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Compare value in array: %d\n", array[i]);
-		
 		if (array[i] == value)
 		{
 			return (i);
